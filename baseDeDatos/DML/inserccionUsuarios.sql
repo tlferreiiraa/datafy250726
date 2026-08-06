2b3376ba75fdf15dfdc697239ab79ec540465d87
THIAGO LUCAS FERREIRA RUIZ <57752678@ceibal.edu.uy>
2026-08-05 21:28:38 -0300

    Actividad de Logueo con Base de Datos - Punto 1

---
+++ baseDeDatos/DML/inserccionUsuarios.sql
@@ -0,0 +1,20 @@
+INSERT INTO USUARIO (Cedula, Nombre, Apellido, Contrasena, Estado, CedulaCreador)
+VALUES ('11111111', 'Ana', 'Pereira', '$2y$10$l0U9nK30ZfeO.BQDohX/VOkRfwjwtnDwXcu9qcyD//6Zpd.Deg4Wy', TRUE, NULL);
+INSERT INTO DOCENTE (Cedula) VALUES ('11111111');
+
+INSERT INTO USUARIO (Cedula, Nombre, Apellido, Contrasena, Estado, CedulaCreador)
+VALUES ('22222222', 'Bruno', 'Silva', '$2y$10$l0U9nK30ZfeO.BQDohX/VOkRfwjwtnDwXcu9qcyD//6Zpd.Deg4Wy', TRUE, NULL);
+INSERT INTO ADMINISTRATIVO (Cedula) VALUES ('22222222');
+
+INSERT INTO USUARIO (Cedula, Nombre, Apellido, Contrasena, Estado, CedulaCreador)
+VALUES ('33333333', 'Carla', 'Gómez', '$2y$10$l0U9nK30ZfeO.BQDohX/VOkRfwjwtnDwXcu9qcyD//6Zpd.Deg4Wy', TRUE, NULL);
+INSERT INTO TECNICO (Cedula) VALUES ('33333333');
+
+INSERT INTO USUARIO (Cedula, Nombre, Apellido, Contrasena, Estado, CedulaCreador)
+VALUES ('44444444', 'Diego', 'Fernández', '$2y$10$l0U9nK30ZfeO.BQDohX/VOkRfwjwtnDwXcu9qcyD//6Zpd.Deg4Wy', TRUE, NULL);
+INSERT INTO DIRECCION (Cedula) VALUES ('44444444');
+
+INSERT INTO USUARIO (Cedula, Nombre, Apellido, Contrasena, Estado, CedulaCreador)
+VALUES ('55555555', 'Elena', 'Rodríguez', '$2y$10$l0U9nK30ZfeO.BQDohX/VOkRfwjwtnDwXcu9qcyD//6Zpd.Deg4Wy', TRUE, NULL);
+INSERT INTO TECNICO (Cedula) VALUES ('55555555');
+INSERT INTO DIRECCION (Cedula) VALUES ('55555555');
